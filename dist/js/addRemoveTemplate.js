ac062446e808be9e37301c08484e54e95842da11
Ugesh2204 <ugesh.roopchand@hotmail.com>
2021-02-03 14:56:57 +0400

    Template items add remove sub item

--- dist/js/addRemoveTemplate.js
+++ dist/js/addRemoveTemplate.js
@@ -1,33 +1,47 @@
-var x = 0; //initlal text box count
-
 function addtempfield(){
-
-    //x++;
     $(".agenda-wrapper").append(
          ` 
          <li>
-                <div class="form-group mt-2 d-flex align-items-center">
-                    <input type="text" class="form-control">
-                    <i class="fas fa-times ml-2 delete-input remove_field"></i> 
-                </div>
+            <div class="form-group mt-2 d-flex align-items-center">
+                <input type="text" class="form-control">
+                <i class="fas fa-times ml-2 delete-input remove_field"></i> 
+            </div>
 
-                <ol class="wrapper-sub">
-                
-                </ol>
+            
+            <ol class="wrapper-sub">
+                <i class="fas fa-plus-circle add-more"></i>
+            
+            </ol>
                 
         </li>
         `
     );
 
-
-
-
-  
-    
 	$(".agenda-wrapper").on("click",".remove_field", function(e){ 
-        e.preventDefault();
+       e.preventDefault();
        $(this).parents('li').remove();
-       //alert("test");
        })
 }
+
+
+
+$(document).on("click", ".add-more", function(e){ //on add input button click
+        e.preventDefault();
+
+        $(this).parent().append(
+                `<li class="rm">
+                    <div class="form-group mt-2 d-flex align-items-center">
+                    <input type="text" class="form-control col-md-10">
+                    <i class="fas fa-times ml-2 delete-sub delete-input"></i>
+                    </div>
+                </li>`
+        )
+
+        $(".wrapper-sub").on("click",".delete-sub", function(e){ 
+            e.preventDefault();
+            $(this).parent().parent('li').remove();
+            //alert("test");
+        });
+
+    });
 
